51618d19f37d499e3f837b69a395d5f1e055d46d
ChertkovFedor <ChertkovFedor@yandex.ru>
2023-07-19 00:54:00 +0300

    2 этап дипломной работы

--- ewm-service/src/main/java/ru/practicum/ewm/EwmServiceApplication.java
+++ ewm-service/src/main/java/ru/practicum/ewm/EwmServiceApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication(scanBasePackages = "ru.practicum")
 public class EwmServiceApplication {
     public static void main(String[] args) {
+
         SpringApplication.run(EwmServiceApplication.class, args);
+
     }
 }
